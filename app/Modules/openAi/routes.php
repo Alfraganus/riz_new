d587aed4a97a4a5d0ea6882a8112fad027f5dc0e
Gulomjon Sulaymonov <uzpsychologist@gmail.com>
2024-09-13 23:40:08 +0500

    initial

--- app/Modules/openAi/routes.php
+++ app/Modules/openAi/routes.php
@@ -7,4 +7,6 @@ use Illuminate\Support\Facades\Route;
 Route::post('/get-gpt-advice-by-image', [OpenAiController::class, 'gptAdviceFromImage']);
 Route::post('/get-gpt-advice-by-text', [OpenAiController::class, 'gptAdviceFromText']);
 Route::post('/get-gpt-random-pick', [OpenAiController::class, 'gptRandomPick']);
+
 Route::post('/get-glens', [OpenAiController::class, 'gptGlens']);
+
